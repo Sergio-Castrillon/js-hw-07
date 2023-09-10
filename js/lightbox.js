77508195ee9bb2402852f2c6a149059fef01a49f
Sergio-Castrillon <115659136+Sergio-Castrillon@users.noreply.github.com>
2023-09-10 17:58:13 -0500

    Commit

--- js/lightbox.js
+++ js/lightbox.js
@@ -16,3 +16,4 @@ let lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
  });
+ 
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const block_1 = document.querySelector('.link-1');
+const block_2 = document.querySelector('.link-2');
+
+
+block_1.addEventListener('click', handleclick = () => {
+  window.open('https://sergio-castrillon.github.io/js-hw-07/gallery.html')
+})
+block_2.addEventListener('click', handleclick = () => {
+  window.open('https://sergio-castrillon.github.io/js-hw-07/lightbox.html')
+})
